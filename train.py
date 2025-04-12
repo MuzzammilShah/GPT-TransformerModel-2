13122c317e8888d9dc42ceabdea3a122109c9c35
MuzzammilShah <muhammed.muzzammilshah@gmail.com>
2025-04-12 10:44:24 +0530

    Declaration of the intial skeleton structure of the GPT 2 architecture, done

--- train.py
+++ train.py
@@ -18,4 +18,10 @@ class GPT(nn.Module):
         super().__init__()
         self.config = config
 
-    
+        self.transformer = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd),
+            wpe = nn.Embedding(config.block_size, config.n_emb),
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+            ln_f = nn.LayerNorm(config.n_emb)
+        ))
+        self.lm_head = nn.Linear(config.vocab_size, config.n_embd, bias=False)
