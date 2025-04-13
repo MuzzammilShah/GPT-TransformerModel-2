a17f79699e2c8536ac8caa6d5de8e0b39f33fbd6
MuzzammilShah <muhammed.muzzammilshah@gmail.com>
2025-04-13 12:21:52 +0530

    Self Attention class implemented only

--- train.py
+++ train.py
@@ -1,9 +1,45 @@
+import math
 from dataclasses import dataclass
 import torch
 from torch import nn
 from torch.nn import functional as F
 
 #=========================================================
+
+class CasualSelfAttention(nn.Module):
+    def __init__(self, config):
+        super.__init__()
+
+        assert config.n_embd % config.n_head == 0
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                             .view(1, 1, config.block_size, config.block_size))
+
+    def forward(self, x):
+        B, T, C = x.size()
+
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+
+        attn = (q @ k.transpose[-2, -1]) * (1.0 / math.sqrt(k.size[-1]))
+
+        attn = attn.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+
+        attn = F.softmax(attn, dim=1)
+
+        y = attn @ v
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+        y = self.c_proj(y)
+
+        return y
 
 class MLP(nn.Module):
     def __init__(self, config):
