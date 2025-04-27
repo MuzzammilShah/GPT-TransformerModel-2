45852a6775e1284f0cb59716152bae206995dc0b
MuzzammilShah <muhammed.muzzammilshah@gmail.com>
2025-04-27 14:12:20 +0530

    Sample output recieved from model! Implementation, Explaination and debugging done

--- train.py
+++ train.py
@@ -29,11 +29,11 @@ class CasualSelfAttention(nn.Module):
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
 
-        attn = (q @ k.transpose[-2, -1]) * (1.0 / math.sqrt(k.size[-1]))
+        attn = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
 
         attn = attn.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
 
-        attn = F.softmax(attn, dim=1)
+        attn = F.softmax(attn, dim=-1)
 
         y = attn @ v
         y = y.transpose(1, 2).contiguous().view(B, T, C)
@@ -102,11 +102,11 @@ class GPT(nn.Module):
         x = tok_emb + pos_emd
 
         for block in self.transformer.h:
-            block(x)
+            x = block(x)
 
         x = self.transformer.ln_f(x)
 
-        logits = self.transformer.lm_head(x)
+        logits = self.lm_head(x)
 
         return logits
     
@@ -167,5 +167,43 @@ class GPT(nn.Module):
 
         return model
     
-model = GPT.from_pretrained('gpt2')
-print("didn't crash yay!")
+# model = GPT.from_pretrained('gpt2')
+# print("didn't crash yay!")
+
+num_return_sequences = 5
+max_length = 30
+
+model = GPT.from_pretrained("gpt2")
+model.eval()
+model.to("cuda")
+
+import tiktoken
+
+enc = tiktoken.get_encoding("gpt2")
+tokens = enc.encode("Hello, I'm a language model,")
+tokens = torch.tensor(tokens, dtype=torch.long)
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
+x = tokens.to("cuda")
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+
+while x.size(1) < max_length:
+
+    with torch.no_grad():
+
+        logits = model(x)
+        logits = logits[:, -1, :]
+        probs = F.softmax(logits, dim=-1)
+
+        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+        ix = torch.multinomial(topk_probs, 1)
+        xcol = torch.gather(topk_indices, -1, ix)
+
+        x = torch.cat((x, xcol), dim=-1)
+
+for i in range(num_return_sequences):
+
+    tokens = x[i, :max_length].tolist()
+    decoded = enc.decode(tokens)
+    print(">", decoded)
