6aa7ba7b75678fdae59da623d2c8bbab358c92f3
MuzzammilShah <muhammed.muzzammilshah@gmail.com>
2025-05-11 18:10:43 +0530

    weight sharing scheme implemented and explained

--- train.py
+++ train.py
@@ -91,6 +91,8 @@ class GPT(nn.Module):
         ))
         self.lm_head = nn.Linear( config.n_embd, config.vocab_size, bias=False)
 
+        self.transformer.wte.weight = self.lm_head.weight
+
     def forward(self, idx, targets=None):
 
         B, T = idx.size()
